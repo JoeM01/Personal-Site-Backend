01d61c7272790e92912aa45dd1b01dddcfd264bb
Blasphemous-Cardinal <blasphemouscardinal@gmail.com>
2024-05-18 11:43:46 +0000

    split up tf files configured load balancer modules.

---
+++ eks.tf
@@ -0,0 +1,65 @@
+module "eks" {
+  source = "terraform-aws-modules/eks/aws"
+
+  cluster_name                   = "Test_Cluster"
+  cluster_endpoint_public_access = true
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_irsa = true
+
+  eks_managed_node_groups = {
+    test-cluster-wg = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+
+      instance_types = ["t3.small"]
+      capacity_type  = "SPOT"
+    }
+  }
+
+
+  enable_cluster_creator_admin_permissions = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+
+}
+
+data "aws_eks_cluster" "default" {
+  name = module.eks.cluster_name
+
+  depends_on = [ module.eks ]
+}
+
+data "aws_eks_cluster_auth" "default" {
+  name = module.eks.cluster_name
+  depends_on = [ module.eks ]
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.default.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.id]
+    command     = "aws"
+  }
+}
