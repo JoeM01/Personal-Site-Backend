52d8e078ddf716d8c8809abdbc933f34798abaa5
Blasphemous-Cardinal <blasphemouscardinal@gmail.com>
2024-05-21 19:30:58 +0000

    added api key for now

--- lambda-function.tf
+++ lambda-function.tf
@@ -3,9 +3,14 @@ module "lambda_function" {
 
   function_name = "Lang-Chain-Handler"
   description   = "Function handling logic for Langchain/OpenAi integration"
+  handler       = "index.lambda_handler"
   runtime       = "nodejs20.x"
 
     create_package      = false
   local_existing_package = "./lambda_function.zip"
+
+  environment_variables = {
+    OPENAI_API_KEY = "sk-proj-arcogGyODm7CMuDFXCbeT3BlbkFJKwF20h9lYPsRQmETvTI9"
+  }
 
 }
