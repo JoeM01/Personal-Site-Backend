4adaff47ffe36a67109428219a88eb86a1528523
Blasphemous-Cardinal <blasphemouscardinal@gmail.com>
2024-05-22 20:32:56 +0000

    changed from local-exec to packaging in github action

--- s3-for-lambda-func.tf
+++ s3-for-lambda-func.tf
@@ -2,19 +2,9 @@ resource "aws_s3_bucket" "lambda_bucket" {
   bucket = "lambda-deployment-bucket-for-langchain"
 }
 
-resource "null_resource" "package_lambda_function" {
-  provisioner "local-exec" {
-    command     = "./package-lambda.sh"
-    interpreter = ["bash", "-c"]
-  }
-
-}
-
-
 resource "aws_s3_object" "lambda_package" {
   bucket = aws_s3_bucket.lambda_bucket.bucket
   key    = "lambda_function.zip"
   source = "lambda_function.zip"
 
-  depends_on = [null_resource.package_lambda_function]
 }
