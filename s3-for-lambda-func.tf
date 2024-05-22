7863ef695eccc48e2fa79efb8c0c7ab1993c180e
Blasphemous-Cardinal <blasphemouscardinal@gmail.com>
2024-05-22 20:36:26 +0000

    chaing object source?

--- s3-for-lambda-func.tf
+++ s3-for-lambda-func.tf
@@ -5,6 +5,6 @@ resource "aws_s3_bucket" "lambda_bucket" {
 resource "aws_s3_object" "lambda_package" {
   bucket = aws_s3_bucket.lambda_bucket.bucket
   key    = "lambda_function.zip"
-  source = "lambda_function.zip"
+  source = "./lambda_function.zip"
 
 }
