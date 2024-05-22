810c3dc51dab243c2cb015c5a04e080ec6a0a007
Blasphemous-Cardinal <blasphemouscardinal@gmail.com>
2024-05-22 20:12:04 +0000

    testing the script

--- s3-for-lambda-func.tf
+++ s3-for-lambda-func.tf
@@ -10,6 +10,7 @@ resource "null_resource" "package_lambda_function" {
 
 }
 
+
 resource "aws_s3_object" "lambda_package" {
   bucket = aws_s3_bucket.lambda_bucket.bucket
   key    = "lambda_function.zip"
