6e08f94db29740ed0883ef1a3204831dcca65849
Blasphemous-Cardinal <blasphemouscardinal@gmail.com>
2024-05-22 19:51:36 +0000

    gave exec perms to script

--- s3-for-lambda-func.tf
+++ s3-for-lambda-func.tf
@@ -2,11 +2,14 @@ resource "aws_s3_bucket" "lambda_bucket" {
   bucket = "lambda-deployment-bucket"
 }
 
+
+
 resource "null_resource" "package_lambda_function" {
   provisioner "local-exec" {
     command     = "./package-lambda.sh"
     interpreter = ["bash", "-c"]
   }
+
 }
 
 resource "aws_s3_object" "lambda_package" {
